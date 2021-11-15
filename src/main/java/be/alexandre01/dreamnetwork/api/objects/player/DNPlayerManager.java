a632c165459aa1cbf404fbc9fa6f4fdbedd4534b
Alexandre01Dev <altata01@hotmail.com>
2021-11-15 12:51:49 +0100

    Little update

---
+++ src/main/java/be/alexandre01/dreamnetwork/api/objects/player/DNPlayerManager.java
@@ -0,0 +1,10 @@
+package be.alexandre01.dreamnetwork.api.objects.player;
+
+import java.util.HashMap;
+
+public class DNPlayerManager {
+    private HashMap<Integer,DNPlayer> dnPlayers = new HashMap<>();
+    public DNPlayerManager(){
+
+    }
+}
