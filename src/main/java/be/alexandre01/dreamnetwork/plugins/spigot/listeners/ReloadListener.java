bbac2854bd6a30fa4c2dd99f3f2ca2df06553045
Alexandre01Dev <altata01@hotmail.com>
2022-04-16 17:37:10 +0200

    Reload Command Patch

--- src/main/java/be/alexandre01/dreamnetwork/plugins/spigot/listeners/ReloadListener.java
+++ src/main/java/be/alexandre01/dreamnetwork/plugins/spigot/listeners/ReloadListener.java
@@ -11,13 +11,12 @@ public class ReloadListener implements Listener {
     @EventHandler
     public void onReload(PlayerCommandPreprocessEvent event){
         String cmd = event.getMessage();
-        if(!event.getPlayer().hasPermission("network.reload")){
-            event.getPlayer().sendMessage("§cVous n'avez pas la permission d'accèder à cette commande.");
-            event.setCancelled(true);
-            return;
-        }
-
         if(cmd.equalsIgnoreCase("/reload") || cmd.equalsIgnoreCase("/rl") || cmd.equalsIgnoreCase("/reloads") ){
+            if(!event.getPlayer().hasPermission("network.reload")){
+                event.getPlayer().sendMessage("§cVous n'avez pas la permission d'accèder à cette commande.");
+                event.setCancelled(true);
+                return;
+            }
             event.setCancelled(true);
             DNSpigot.getInstance().isReloading = true;
             event.getPlayer().sendMessage("§e>> DreamNetwork: §cLa commande /reload n'est pas conseillé, elle peut rentrer en conflit avec d'autres plugins et causer des fuites de mémoire! A utiliser avec précaution");
